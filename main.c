80083aa090a203d4368376f4fc7f2f8db8dbca9c
SoryeongJeong <55128033+SoryeongJeong@users.noreply.github.com>
2019-11-05 15:40:59 +0900

    op_3(2)

--- main.c
+++ main.c
@@ -6,24 +6,18 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	int grade[SIZE];
-	int score[SIZE];
-	
-	for(i=0;i<SIZE;i++)
-	{
-		grade[i]=rand()%100;
-	
-	}
-	
-	for(i=0;i<SIZE;i++)
-	{		
-		score[i] = grade[i];
-	}
+	int a[SIZE] = {1,2,3,4,5};
+	int b[SIZE] = {1,2,3,4,6};
 		
+	
+	
 	for(i=0;i<SIZE;i++)
 	{
-		printf("score[%d] = %d\n", i, score[i]);
+		if (a[i] != b[i])
+		printf("%d is different", i);
+	
 	}
+	
 	
 	return 0;
 }
